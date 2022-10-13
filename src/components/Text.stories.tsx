47c3effca42dfc4367df3707d2c4b5e43cc5d9b2
Adler Coelho <adler.coelho.santos@icloud.com>
2022-10-13 07:48:58 -0300

    feat: finishing Text component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,48 @@
-import { Text } from './Text';
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
 
 export default {
     title: 'Components/Text',
-    component: Text
+    component: Text,
+    args: {
+        children: 'Adler Lab',
+        size: 'md'
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: { type: 'inline-radio' }
+        }
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
 }
 
-export const Default = {}
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        size: 'lg',
+        asChild: true,
+        children: (
+            <p>Teste</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        }
+    }
+}
