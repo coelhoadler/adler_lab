2c5de28441faffd8df010258f5c9a1997f44b103
Adler Coelho <adler.coelho.santos@icloud.com>
2022-10-13 08:00:29 -0300

    feat: Heading component

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,50 +1,50 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Text, TextProps } from './Text';
+import { Heading, HeadingProps } from './Heading';
 
 export default {
-    title: 'Components/Text',
-    component: Text,
+    title: 'Components/Heading',
+    component: Heading,
     args: {
-        children: 'Adler Lab',
+        children: 'Meu Heading',
         size: 'md'
     },
     argTypes: {
         size: {
             options: ['sm', 'md', 'lg'],
             control: { type: 'inline-radio' }
-        },
-        asChild: {
-            table: {
-                disable: true
-            }
-        }        
+        }
     }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
     args: {
         size: 'sm'
     }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
     args: {
         size: 'lg'
     }
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
     args: {
         size: 'lg',
         asChild: true,
         children: (
-            <p>Teste</p>
+            <h1>Heading with h1</h1>
         )
     },
     argTypes: {
         children: {
+            table: {
+                disable: true
+            }
+        },
+        asChild: {
             table: {
                 disable: true
             }
